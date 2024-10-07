9413ed001bc3cdfd6abc0fb64a660c6cef88e499
Lydia-ma-sketch <kk.lydia@gmail.com>
2024-10-07 12:25:44 +0630

    Added javascript to update date and time according to different time zones

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,28 @@
-
+function updateTime() {
+  //london
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector("#date");
+    let londonTimeElement = londonElement.querySelector("#time");
+    londonDateElement.innerHTML = moment()
+      .tz("Europe/London")
+      .format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = moment()
+      .tz("Europe/London")
+      .format("H:mm:ss [<small>]A[</small>]");
+  }
+  //new-york
+  let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector("#date");
+    let newYorkTimeElement = newYorkElement.querySelector("#time");
+    newYorkDateElement.innerHTML = moment()
+      .tz("America/New_York")
+      .format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = moment()
+      .tz("America/New_York")
+      .format("H:mm:ss [<small>]A[</small>]");
+  }
+}
+updateTime();
+setInterval(updateTime, 1000);
